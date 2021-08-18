18c5d633b3c3ec51cd2f70e1a9c6f3cec1e26879
sabbirlog <mridhasabbir5@gmail.com>
2021-08-18 22:39:15 +0600

    feat: medium and small card sections

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Head from 'next/head';
 import Banner from '../components/Banner';
 import Header from '../components/Header';
+import MediumCard from '../components/MediumCard';
+import SmallCard from '../components/SmallCard';
 
-export default function Home() {
+export default function Home({ exploreData, mediumCardsData }) {
+  console.log(exploreData);
+
   return (
     <div className=''>
       <Head>
@@ -11,6 +15,50 @@ export default function Home() {
       </Head>
       <Header />
       <Banner />
+      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+        <section className='pt-6'>
+          <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+
+          {/** pull data from api endpoints */}
+          <div className='grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+            {exploreData?.map(({ img, location, distance }, i) => (
+              <SmallCard
+                key={i}
+                img={img}
+                location={location}
+                distance={distance}
+              />
+            ))}
+          </div>
+        </section>
+        <section>
+          <h2 className='text-4xl font-semibold py-8'>Live Anywhere</h2>
+
+          {/** pull data from api endpoints */}
+          <div className='flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3'>
+            {mediumCardsData?.map(({ img, title }, i) => (
+              <MediumCard key={i} img={img} title={title} />
+            ))}
+          </div>
+        </section>
+      </main>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const exploreData = await fetch('https://links.papareact.com/pyp').then(
+    (res) => res.json()
+  );
+
+  const mediumCardsData = await fetch('https://links.papareact.com/zp1').then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      exploreData,
+      mediumCardsData,
+    },
+  };
 }
